3f343d896852de4f58f2c294a97a7ff19084c934
Mika4vondenen <moneyprintercrp@gmail.com>
2025-11-20 16:44:24 +0100

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,10 +3,9 @@ import { useState, useEffect } from 'react';
 
 export default function Hero() {
   const images = [
-    'https://images.pexels.com/photos/3184296/pexels-photo-3184296.jpeg',
-    'https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg',
-    'https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg',
-    'https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg',
+    '/assets/1d26fb92-f725-4f82-8838-d35c9c7fe8b1.png',
+    '/assets/982564cf-d447-492e-a99f-f592f3f4d981.png',
+    '/assets/f61c5e57-a3c5-4a50-99e0-637f33fef76b.png',
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
