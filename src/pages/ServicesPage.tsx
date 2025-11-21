da0236a81ea9a81f6371e4863796a54271e1b6ab
Mika4vondenen <moneyprintercrp@gmail.com>
2025-11-21 22:41:54 +0100

    Updated ServicesPage.tsx

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -58,11 +58,14 @@ export default function ServicesPage() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-16">
-            <div className="bg-gradient-to-br from-[#111111] to-[#181818] p-12 rounded-xl">
+            <div className="bg-gradient-to-br from-[#111111] to-[#181818] p-12 rounded-xl flex flex-col items-center">
+              <img src="/assets/5dcbd8e8-73be-4b58-8b97-90007764cead.png" alt="icon" className="h-16 w-16 mb-6" />
             </div>
-            <div className="bg-gradient-to-br from-[#111111] to-[#181818] p-12 rounded-xl">
+            <div className="bg-gradient-to-br from-[#111111] to-[#181818] p-12 rounded-xl flex flex-col items-center">
+              <img src="/assets/5dcbd8e8-73be-4b58-8b97-90007764cead.png" alt="icon" className="h-16 w-16 mb-6" />
             </div>
-            <div className="bg-gradient-to-br from-[#111111] to-[#181818] p-12 rounded-xl">
+            <div className="bg-gradient-to-br from-[#111111] to-[#181818] p-12 rounded-xl flex flex-col items-center">
+              <img src="/assets/5dcbd8e8-73be-4b58-8b97-90007764cead.png" alt="icon" className="h-16 w-16 mb-6" />
             </div>
           </div>
 
