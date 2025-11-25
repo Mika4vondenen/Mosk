b8f278e0ebefe1147122d1e09d6a3f9358b152df
Mika4vondenen <moneyprintercrp@gmail.com>
2025-11-25 12:38:24 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import Hero from './components/Hero';
 import Contact from './components/Contact';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import CookieBanner from './components/CookieBanner';
 import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import PortfolioPage from './pages/PortfolioPage';
+import ImpressumPage from './pages/ImpressumPage';
+import DatenschutzPage from './pages/DatenschutzPage';
 import { AnimationProvider } from './context/AnimationContext';
 
 function App() {
@@ -27,8 +30,11 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/services" element={<ServicesPage />} />
             <Route path="/portfolio" element={<PortfolioPage />} />
+            <Route path="/impressum" element={<ImpressumPage />} />
+            <Route path="/datenschutz" element={<DatenschutzPage />} />
           </Routes>
           <Footer />
+          <CookieBanner />
         </div>
       </Router>
     </AnimationProvider>
